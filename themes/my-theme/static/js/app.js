f5509c7845305dcb91821aee8bf2a5499957864c
Zdenek Pribyla <zdenek.pribyla@gmail.com>
2018-04-17 09:04:43 +0200

    add "product order" into table (JS)
    The Table still get data of all products in yaml. The next target is getting data just from choosen products.

--- themes/my-theme/static/js/app.js
+++ themes/my-theme/static/js/app.js
@@ -4,7 +4,20 @@ var myFunction = function () {
         var oneClass = x[i]
         if (x[i].className === "navbar__li") {
             x[i].className += " show-menu"
-                    }
-        else {x[i].className = "navbar__li"}
+        }
+        else { x[i].className = "navbar__li" }
     }
 }
+
+var productOrder = function () {
+    var element = document.getElementsByClassName("number-order-in-table");
+    for (var i = 0; i < element.length; i++) {
+        var myVar = i + 1;
+        console.log(myVar)
+        element[i].insertAdjacentHTML('afterbegin', myVar)
+        }
+}
+productOrder()
+
+
+
